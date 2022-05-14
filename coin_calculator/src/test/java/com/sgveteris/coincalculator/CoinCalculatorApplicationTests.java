4e31f8b0f9ab1fdf5c900eb0769e9f1bfac150a3
Sevim Demir <sevimmdemir@gmail.com>
2022-05-14 15:29:38 +0300

    first commit

---
+++ coin_calculator/src/test/java/com/sgveteris/coincalculator/CoinCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgveteris.coincalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
