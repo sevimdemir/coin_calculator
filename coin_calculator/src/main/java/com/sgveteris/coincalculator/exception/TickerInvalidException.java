21ab9923bab80be09c9e3ba73691fdae3119d83e
Sevim Demir <sevimmdemir@gmail.com>
2022-05-16 12:28:26 +0300

    file structure fix

--- coin_calculator/src/main/java/com/sgveteris/coincalculator/exception/TickerInvalidException.java
+++
@@ -1,9 +0,0 @@
-package com.sgveteris.coincalculator.exception;
-
-public class TickerInvalidException extends Exception {
-
-    public TickerInvalidException(String symbol) {
-        super(symbol.concat(" is invalid"));
-    }
-
-}
--- coin_calculator/src/main/java/com/sgveteris/coincalculator/exception/TickerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.sgveteris.coincalculator.exception;
-
-public class TickerNotFoundException extends Exception{
-
-    public TickerNotFoundException(String symbol){
-        super("Cannot find the response for ".concat(symbol));
-    }
-
-}
